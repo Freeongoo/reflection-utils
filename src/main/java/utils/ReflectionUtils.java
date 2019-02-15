65a08fdb10dbe2c2c8c86c216d79edecdc2e8ce7
dorofeev <v.dorofeev@tetra-soft.ru>
2019-02-15 11:40:14 +0300

    add additional method for cast value of field

--- src/main/java/utils/ReflectionUtils.java
+++ src/main/java/utils/ReflectionUtils.java
@@ -14,6 +14,10 @@ public class ReflectionUtils {
     private static final String GETTER_PREFIX = "get";
     private static final String SETTER_PREFIX = "set";
 
+    /**
+     * @param fieldName fieldName
+     * @return getter name
+     */
     public static String getterByFieldName(String fieldName) {
         if (isStringNullOrEmpty(fieldName))
             return null;
@@ -21,6 +25,10 @@ public class ReflectionUtils {
         return convertFieldByAddingPrefix(fieldName, GETTER_PREFIX);
     }
 
+    /**
+     * @param fieldName fieldName
+     * @return setter name
+     */
     public static String setterByFieldName(String fieldName) {
         if (isStringNullOrEmpty(fieldName))
             return null;
@@ -28,10 +36,12 @@ public class ReflectionUtils {
         return convertFieldByAddingPrefix(fieldName, SETTER_PREFIX);
     }
 
-    private static String convertFieldByAddingPrefix(String fieldName, String prefix) {
-        return prefix + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-    }
-
+    /**
+     *
+     * @param obj obj
+     * @param fieldName fieldName
+     * @return content of field
+     */
     public static Object getFieldContent(Object obj, String fieldName) {
         if (!isValidParams(obj, fieldName))
             return null;
@@ -44,6 +54,11 @@ public class ReflectionUtils {
         }
     }
 
+    /**
+     * @param obj obj
+     * @param fieldName fieldName
+     * @param value value
+     */
     public static void setFieldContent(Object obj, String fieldName, Object value) {
         if (!isValidParams(obj, fieldName))
             return;
@@ -56,16 +71,11 @@ public class ReflectionUtils {
         }
     }
 
-    private static Field getFieldAccessible(Object obj, String fieldName) {
-        Optional<Field> optionalField = getField(obj, fieldName);
-        return optionalField
-                .map(el -> {
-                    el.setAccessible(true);
-                    return el;
-                })
-                .orElseThrow(() -> new IllegalArgumentException("Cannot find field name: " + fieldName));
-    }
-
+    /**
+     * @param obj obj
+     * @param methodName methodName
+     * @return result of method
+     */
     public static Object callMethod(Object obj, String methodName) {
         if (!isValidParams(obj, methodName))
             return null;
@@ -79,6 +89,10 @@ public class ReflectionUtils {
         }
     }
 
+    /**
+     * @param clazz clazz
+     * @return array of fields
+     */
     public static Field[] getAllFields(Class<?> clazz) {
         if (clazz == null) return null;
 
@@ -90,6 +104,11 @@ public class ReflectionUtils {
         return fields.toArray(new Field[] {});
     }
 
+    /**
+     * @param obj obj
+     * @param fieldName fieldName
+     * @return optional field
+     */
     public static Optional<Field> getField(Object obj, String fieldName) {
         if (!isValidParams(obj, fieldName))
             return Optional.empty();
@@ -98,6 +117,11 @@ public class ReflectionUtils {
         return getField(clazz, fieldName);
     }
 
+    /**
+     * @param clazz clazz
+     * @param fieldName fieldName
+     * @return optional field
+     */
     public static Optional<Field> getField(Class<?> clazz, String fieldName) {
         if (!isValidParams(clazz, fieldName))
             return Optional.empty();
@@ -108,11 +132,60 @@ public class ReflectionUtils {
                 .findFirst();
     }
 
+    /**
+     * @param clazz clazz
+     * @param fieldName fieldName
+     * @param fieldValue fieldValue
+     * @return field value casting by correct type
+     */
+    public static Object castFieldValue(Class<?> clazz, String fieldName, Object fieldValue) {
+        Field field = getField(clazz, fieldName)
+                .orElseThrow(() -> new IllegalArgumentException("Cannot find field name:" + fieldName));
+
+        Class<?> fieldType = field.getType();
+
+        if (fieldType.isAssignableFrom(Double.class)) {
+            return ((Number) fieldValue).doubleValue();
+        }
+
+        if (fieldType.isAssignableFrom(Long.class)) {
+            return ((Number) fieldValue).longValue();
+        }
+
+        if (fieldType.isAssignableFrom(Float.class)) {
+            return ((Number) fieldValue).floatValue();
+        }
+
+        if (fieldType.isAssignableFrom(Integer.class)) {
+            return ((Number) fieldValue).intValue();
+        }
+
+        if (fieldType.isAssignableFrom(Short.class)) {
+            return ((Number) fieldValue).shortValue();
+        }
+
+        return fieldValue;
+    }
+
     private static boolean isValidParams(Object obj, String param) {
         return (obj != null && !isStringNullOrEmpty(param));
     }
 
     private static boolean isStringNullOrEmpty(String fieldName) {
         return fieldName == null || fieldName.trim().length() == 0;
+    }
+
+    private static Field getFieldAccessible(Object obj, String fieldName) {
+        Optional<Field> optionalField = getField(obj, fieldName);
+        return optionalField
+                .map(el -> {
+                    el.setAccessible(true);
+                    return el;
+                })
+                .orElseThrow(() -> new IllegalArgumentException("Cannot find field name: " + fieldName));
+    }
+
+    private static String convertFieldByAddingPrefix(String fieldName, String prefix) {
+        return prefix + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
     }
 }
